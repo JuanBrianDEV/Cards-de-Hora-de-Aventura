76f1156233f31dfd4bfc88b041d46b6aa1ad2031
JuanBrianDEV <juanbrianm10@gmail.com>
2025-11-28 16:42:37 -0300

    commit inicial

---
+++ script.js
@@ -0,0 +1,17 @@
+const cards = document.querySelectorAll('.card');
+
+cards.forEach(card => {
+    card.addEventListener('click', function() {
+        const season = this.dataset.season;
+        const episode = this.dataset.episode;
+        const title = this.dataset.title;
+        
+        const info = document.getElementById('info');
+        info.innerHTML = `
+            <p><strong>Temporada:</strong> ${season}</p>
+            <p><strong>Episódio:</strong> ${episode}</p>
+            <p><strong>Título:</strong> ${title}</p>
+        `;
+    });
+});
+
